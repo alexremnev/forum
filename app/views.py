50e33622deca9b6640cc637d3078efd62c4c7299
Alex Remniov <alexremneval@gmail.com>
2018-02-26 10:07:09 +0200

    basic templates

--- app/views.py
+++ app/views.py
@@ -1,7 +1,19 @@
+from flask import render_template
+
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('home.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/posts')
+def posts():
+    return render_template('posts.html')
