0ad54483db81a1c02ded968a096b662c80c2c40a
Alex Remniov <alexremneval@gmail.com>
2018-03-02 18:20:14 +0200

    added prerequisites before run

---
+++ ReadMe.txt
@@ -0,0 +1,3 @@
+Before run the project:
+1) execute: python db_create.py admin test@test.com admin
+
--- db_create.py
+++ db_create.py
@@ -1,3 +1,23 @@
 from app import db
+from app.models import Role, User
+import sys
+from passlib.handlers.sha2_crypt import sha256_crypt
 
-db.create_all()
+for arg in [sys.argv]:
+    try:
+        username = arg[1]
+        email = arg[2]
+        password = arg[3]
+        db.create_all()
+        roles = ['user', 'admin', 'moderator']
+        for name in roles:
+            new_role = Role(name=name)
+            db.session.add(new_role)
+        admin_roles = Role.query.all()
+        admin = User(username=username, email=email, password=sha256_crypt.encrypt(password), roles=admin_roles)
+        db.session.add(admin)
+        db.session.commit()
+    except:
+        db.session.rollback()
+        print("Invalid args")
+        break
