9f98a8039b66cdaf28a771bca339ca9471af20b3
Alex Remniov <alexremneval@gmail.com>
2018-03-04 13:33:28 +0200

    added validation on duplicate

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, TextAreaField
-from wtforms.validators import InputRequired, Length, Email
+from wtforms.validators import InputRequired, Length, Email, DataRequired
+
+from app.models import User, Post
 
 
 class LoginForm(FlaskForm):
@@ -10,14 +12,41 @@ class LoginForm(FlaskForm):
 
 
 class RegisterForm(FlaskForm):
-    username = StringField('username', validators=[InputRequired(), Length(min=1, max=30)])
-    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    username = StringField('username', validators=[InputRequired(), DataRequired(), Length(min=1, max=30)])
+    email = StringField('email', validators=[InputRequired(), DataRequired(), Email(message='Invalid email'), Length(max=50)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=3, max=100)])
+
+    def validate(self):
+        form = FlaskForm.validate(self)
+        if not form:
+            return False
+
+        user = User.query.filter_by(username=self.username.data).first()
+        if user is not None:
+            self.username.errors.append('Nickname already exists')
+            return False
+
+        user = User.query.filter_by(email=self.email.data).first()
+        if user is not None:
+            self.email.errors.append('Email already exists')
+            return False
+        return True
 
 
 class PostForm(FlaskForm):
-    title = StringField('title', validators=[InputRequired(), Length(min=1, max=50)])
+    title = StringField('title', validators=[InputRequired(), DataRequired(), Length(min=1, max=50)])
     body = TextAreaField('body', validators=[InputRequired(), Length(max=5000)])
+
+    def validate(self):
+        form = FlaskForm.validate(self)
+        if not form:
+            return False
+
+        post = Post.query.filter_by(title=self.title.data).first()
+        if post is not None:
+            self.title.errors.append('Title already exists')
+            return False
+        return True
 
 
 class CommentForm(FlaskForm):
